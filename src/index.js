c5d74de4b943f13d21cc28b790d1a05a0eb6266d
nykoh2001 <nykoh2001@gmail.com>
2022-12-12 14:20:26 +0900

    chore: route edit + extension

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import AppearAni from "./AppearAni";
+import AppearAni from "./components/AppearAni";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
